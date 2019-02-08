961fc4af6297f8e79c3f3b47458c964f05693fd0
Evan <evanslin@gmail.com>
2019-02-09 02:53:01 +0800

    remove GetWebLoinURL

--- tools.go
+++ tools.go
@@ -9,7 +9,6 @@ import (
 	"math/rand"
 	"net/http"
 	"net/url"
-	"os"
 	"strings"
 	"time"
 )
@@ -46,29 +45,6 @@ func randStringRunes(n int) string {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
 	return string(b)
-}
-
-//GetWebLoinURL - LINE LOGIN 2.1 get LINE Login URL
-func GetWebLoinURL(clientID, redirectURL, state, scope, nounce, chatbotPrompt string) string {
-	req, err := http.NewRequest("GET", "https://access.line.me/oauth2/v2.1/authorize", nil)
-	if err != nil {
-		log.Print(err)
-		os.Exit(1)
-	}
-	q := req.URL.Query()
-	q.Add("response_type", "code")
-	q.Add("client_id", clientID)
-	q.Add("state", state)
-	q.Add("scope", scope)
-	q.Add("nounce", nounce)
-	q.Add("redirect_uri", redirectURL)
-	if len(chatbotPrompt) > 0 {
-		q.Add("bot_prompt", chatbotPrompt)
-	}
-	q.Add("prompt", "consent")
-	req.URL.RawQuery = q.Encode()
-	log.Println(req.URL.String())
-	return req.URL.String()
 }
 
 func GenerateNounce() string {
