57bdc9ce6a481320cfef20449e0d4a99ebb2470f
Evan Lin <evanslin@gmail.com>
2021-04-15 15:57:39 +0800

    Remove unuse code

--- line_login.go
+++ line_login.go
@@ -13,8 +13,6 @@ import (
 var nonce string
 var state string
 
-const lineLoginURL string = "https://access.line.me/oauth2/v2.1/authorize?response_type=code"
-
 func browse(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("login.tmpl"))
 	if err := tmpl.Execute(w, nil); err != nil {
